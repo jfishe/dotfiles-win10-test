a44c0ef8fd0a07f3f713cd2f4126f84c7be91702
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-09 10:59:12 -0800

    formatting...

--- PowerShellModuleSkeleton/public/Test-PublicFunction.ps1
+++ PowerShellModuleSkeleton/public/Test-PublicFunction.ps1
@@ -1,17 +1,14 @@
 function Test-PublicFunction {
     [cmdletbinding()]
+    
     param(
         [string]$printSomething = 'something!'
     )
 
-    Begin {}
-
     Process {
+        
         Write-Output "Writing something to output stream: $printSomething"
         Write-Verbose "Writing something to the verbose stream: $printSomething"
-        
+
     }
-
-    End {}
-
 }
