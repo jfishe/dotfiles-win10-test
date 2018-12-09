9367ee69a321b3a5108b015bc3a8a20aca78fcd5
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-09 13:47:49 -0800

    variable typo

--- PowerShellModuleSkeleton/Publish-ToPSGallery.ps1
+++ PowerShellModuleSkeleton/Publish-ToPSGallery.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = 'stop'
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
-Publish-Module -Path $modulerooot -NuGetApiKey 'oy2mlwbhjrgbiu4fwydq3waghwqitb4elfqpu7fredlohi' -Force -Verbose
+Publish-Module -Path $moduleroot -NuGetApiKey 'oy2mlwbhjrgbiu4fwydq3waghwqitb4elfqpu7fredlohi' -Force -Verbose
