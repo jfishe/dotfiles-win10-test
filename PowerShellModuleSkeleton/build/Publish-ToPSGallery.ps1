003a647a2cf3ef4506291bfc94894588bc25d1f8
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-09 13:03:23 -0800

    testing psgallery deployment

--- PowerShellModuleSkeleton/build/Publish-ToPSGallery.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = 'stop'
-Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
-Publish-Module -Name 'PowerShellModuleSkeleton' -path ..\ -NuGetApiKey 'oy2mlwbhjrgbiu4fwydq3waghwqitb4elfqpu7fredlohi' -Force
