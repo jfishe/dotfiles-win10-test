aae3a367b6498c45691d924e55dd12ab40324e9a
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-09 11:52:00 -0800

    build failing on account of bad pester cert, skipping ca check for now

--- PowerShellModuleSkeleton/tests/Invoke-Tests.ps1
+++ PowerShellModuleSkeleton/tests/Invoke-Tests.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'stop'
 Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
-Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false
+Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false -skippublishercheck
 Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false
 Import-Module Pester -Verbose
 Import-Module PSScriptAnalyzer -Verbose
