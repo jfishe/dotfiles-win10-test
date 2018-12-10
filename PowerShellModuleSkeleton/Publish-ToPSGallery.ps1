9918b095c78e311fb205906e56750b92323a6257
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-09 16:02:52 -0800

    testing vsts secret vars

--- PowerShellModuleSkeleton/Publish-ToPSGallery.ps1
+++ PowerShellModuleSkeleton/Publish-ToPSGallery.ps1
@@ -1,4 +1,23 @@
-$ErrorActionPreference = 'stop'
-$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
-Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
-Publish-Module -Path $moduleroot -NuGetApiKey $env:api_key -Force -Verbose
+function Publish-ToPSGallery {
+    [CmdletBinding()]
+
+    param(
+        [Parameter()]
+        [string]$apiKey
+    )
+
+    Process 
+    {
+        try {        
+            $ErrorActionPreference = 'stop'
+            $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+            Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false -ErrorAction Stop
+            Publish-Module -Path $moduleroot -NuGetApiKey $apiKey -Force -Verbose -ErrorAction Stop
+        }
+        catch {
+            throw $_
+        }
+
+    }
+}
+
