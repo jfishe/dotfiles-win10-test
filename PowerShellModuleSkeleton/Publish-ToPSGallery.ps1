bd4104a4bd6e1d5bf8a716c1dcf157bb3e605822
Alek Jouharyan <alekjouharyan@gmail.com>
2018-12-09 16:20:41 -0800

    fixing build

--- PowerShellModuleSkeleton/Publish-ToPSGallery.ps1
+++ PowerShellModuleSkeleton/Publish-ToPSGallery.ps1
@@ -1,29 +1,8 @@
-function Publish-ToPSGallery {
-    [CmdletBinding()]
+param (
+    $apikey
+)      
 
-    param(
-        [Parameter()]
-        [string]$apiKey,
-
-        [Parameter()]
-        [string]$path
-    )
-
-    Process 
-    {
-        try {        
-            $ErrorActionPreference = 'stop'
-            Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false -ErrorAction Stop -Verbose
-            Publish-Module -Path $moduleroot -NuGetApiKey $apiKey -Force -ErrorAction Stop -Verbose
-        }
-        catch {
-            throw $_
-        }
-
-    }
-}
-
-# set location to script's location (module root). Apikey variable is set by task, 
-# since secret variables can't be referenced driectly from the powershell task.
+$ErrorActionPreference = 'stop'
+Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false -ErrorAction Stop -Verbose
 $moduleroot = Split-Path -path $MyInvocation.MyCommand.Path
-Publish-ToPSGallery -path $moduleroot
+Publish-Module -Path $moduleroot -NuGetApiKey $apiKey -Force -ErrorAction Stop -Verbose
